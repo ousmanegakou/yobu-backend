fe832ef4f337c0c0059b5364c514452fed6e56da
ousmanegakou <gakououssou@gmail.com>
2026-03-09 18:51:13 -0400

    feat: YOBU v2 - src/config/twilio.js

---
+++ src/config/twilio.js
@@ -0,0 +1,10 @@
+const twilio = require('twilio');
+const { TWILIO_SID, TWILIO_TOKEN, TWILIO_FROM } = require('./env');
+let client = null;
+if (TWILIO_SID && TWILIO_TOKEN) { client = twilio(TWILIO_SID, TWILIO_TOKEN); }
+else { console.warn('Twilio not configured - SMS disabled'); }
+const sendSMS = async (to, body) => {
+  if (!client) { console.log('[SMS MOCK] To: ' + to + ' | ' + body); return { mock: true }; }
+  return client.messages.create({ from: TWILIO_FROM, to, body });
+};
+module.exports = { sendSMS };
