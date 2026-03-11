98e1fa5ddeabf60322956f8a7b3369da1a0150ca
ousmanegakou <gakououssou@gmail.com>
2026-03-11 00:24:26 -0400

    fix: bind to 0.0.0.0 for Railway

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 require('./config/env');
-const http = require('http');
-const app = require('./app');
-const { setupWebSocket } = require('./services/websocket');
-const { PORT } = require('./config/env');
-const server = http.createServer(app);
-setupWebSocket(server, app);
-server.listen(PORT, () => {
-  console.log(`YOBU Backend running on port ${PORT}`);
-});
-process.on('unhandledRejection', (err) => { console.error(err); server.close(() => process.exit(1)); });
+const http=require('http'),app=require('./app'),{setupWebSocket}=require('./services/websocket'),{PORT}=require('./config/env');
+const server=http.createServer(app);
+setupWebSocket(server,app);
+server.listen(PORT,'0.0.0.0',()=>{console.log('YOBU Backend running on port '+PORT);});
+process.on('unhandledRejection',(e)=>{console.error(e);server.close(()=>process.exit(1));});
